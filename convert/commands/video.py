19d2bc6d35d23d61f4657eea10651996774616c9
PatillaCode <patillacode@gmail.com>
2018-10-23 02:45:37 +0200

    Typo.

--- convert/commands/video.py
+++ convert/commands/video.py
@@ -60,7 +60,7 @@ class Video(Base):
         }
 
     def run(self):
-        """Run the Audio command."""
+        """Run the Video command."""
         chosen_option = let_user_pick(self.conversion_map)
         source_path, output_path, params = self.get_user_input(
             self.conversion_map[chosen_option])
